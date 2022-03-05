add8fa8072e7dc94817131d57d7eb7c39f0268e9
kwakeroni <kwakeroni@gmail.com>
2022-03-05 16:36:07 +0100

    debug upload

--- build_release.sh
+++
@@ -1,9 +0,0 @@
-echo Using snapshot repo: $(echo $SNAPSHOT_REPO | md5sum)
-echo Using  release repo: $(echo $RELEASE_REPO | md5sum)
-echo Using   deploy user: $(echo $OSSRH_USER | md5sum)
-echo Using     deploy pw: $(echo $OSSRH_PASSWORD | md5sum)
-
-./mvnw --batch-mode --update-snapshots \
-  $GOAL -Pcomplete \
-  -DaltSnapshotDeploymentRepository="$SNAPSHOT_REPO" \
-  -DaltReleaseDeploymentRepository="$RELEASE_REPO"
